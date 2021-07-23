4c6ddd0e57ac4f202792957e8eab4521e881a313
saipriya468 <61447721+saipriya468@users.noreply.github.com>
2021-07-23 07:29:48 +0530

    redux storeprovider

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+//import CounterForm from "./form/ClassForm";
+import InForm from "./form/InputForm";
+/*import { Provider } from "react-redux";
+//import Counter from "./counter/Counter";
+import store from "./store";
+import Counter from "./counter/Counter";
+//import {createStore} from "redux"
+<Provider store={store}>
+        <Counter/>
+      </Provider>-->
+     */
+    
+function App() {
+  return (
+    <div>
+<InForm/>
+          </div>
+  );
+}
+
+export default App;
