ba1a7b993133e52dc91883b1f72106168c5726ee
saipriya468 <61447721+saipriya468@users.noreply.github.com>
2021-07-23 07:31:11 +0530

    redux store

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
 import React from "react";
-//import CounterForm from "./form/ClassForm";
-import InForm from "./form/InputForm";
-/*import { Provider } from "react-redux";
-//import Counter from "./counter/Counter";
+import { Provider } from "react-redux";
 import store from "./store";
 import Counter from "./counter/Counter";
-//import {createStore} from "redux"
-<Provider store={store}>
-        <Counter/>
-      </Provider>-->
-     */
+import {createStore} from "redux"
     
 function App() {
   return (
     <div>
-<InForm/>
+          <Provider store={store}>
+        <Counter/>
+      </Provider>
           </div>
   );
 }
